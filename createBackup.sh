1e4e4cb7e8092643caee6b6f4a516633a9eb9042
oliversalzburg <oliver.salzburg@gmail.com>
2011-12-21 14:38:41 +0000

    Switched to google code update URL
    git-svn-id: http://typo3scripts.googlecode.com/svn/branches/dev@9 a8a2147e-0eb0-e98b-0563-54e275c8f842

--- createBackup.sh
+++ createBackup.sh
@@ -45,7 +45,7 @@ DB=*database*
 # Script Configuration end
 
 # The base location from where to retrieve new versions of this script
-UPDATE_BASE=http://hartwig-at.de/fileadmin/t3scripts
+UPDATE_BASE=http://typo3scripts.googlecode.com/svn/trunk
 
 # Self-update
 runSelfUpdate() {
