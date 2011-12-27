d6a7233c4da9d8cebbfe25c10331a34f57cd3dfc
oliversalzburg <oliver.salzburg@gmail.com>
2011-12-27 16:07:22 +0000

    Added ability to look for unzip utility and GraphicsMagick installation to work around usability issues. Please see the following issues for more information: unzip: http://forge.typo3.org/issues/30918 GraphicsMagick: http://forge.typo3.org/issues/32761
    git-svn-id: http://typo3scripts.googlecode.com/svn/branches/dev@42 a8a2147e-0eb0-e98b-0563-54e275c8f842

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 # written by Oliver Salzburg
 #
 # Changelog:
+# 1.2.0 - Added some settings to localconf.php generation
 # 1.1.0 - Code cleaned up
 #         Extended command line paramter support
 #         Improved self-updating
@@ -18,16 +19,18 @@ SELF=$(basename "$0")
 showHelp() {
   cat << EOF
   Usage: $0 [OPTIONS --version=<VERSION>]|<VERSION>
-  
+
   Core:
   --help              Display this help and exit.
   --update            Tries to update the script to the latest version.
   --base=PATH         The name of the base path where Typo3 should be
                       installed. If no base is supplied, "typo3" is used.
-              
+
   Options:
   --version=VERSION   The version to install.
-  
+  --skip-gm-detect    Skips the detection of GraphicsMagick.
+  --skip-unzip-detect Skips the detection of the unzip utility.
+
   Database:
   --hostname=HOST     The name of the host where the Typo3 database is running.
   --username=USER     The username to use when connecting to the Typo3
@@ -35,7 +38,7 @@ showHelp() {
   --password=PASSWORD The password to use when connecting to the Typo3
                       database.
   --database=DB       The name of the database in which Typo3 is stored.
-              
+
   Note: When using an external configuration file, it is sufficient to supply
         just the target version as a parameter.
         When supplying other any command line argument, supply the target
@@ -65,6 +68,10 @@ USER=*username*
 PASS=*password*
 # The name of the database in which Typo3 is stored
 DB=typo3
+# Should the detection of GraphicsMagick be skipped?
+SKIP_GM_DETECT=false
+# Should the detection of the unzip utility be skipped?
+SKIP_UNZIP_DETECT=false
 # Script Configuration end
 
 # Pre-initialize password to random 16-character string if possible
@@ -78,7 +85,7 @@ UPDATE_BASE=http://typo3scripts.googlecode.com/svn/trunk
 # Self-update
 runSelfUpdate() {
   echo "Performing self-update..."
-  
+
   # Download new version
   echo -n "Downloading latest version..."
   if ! wget --quiet --output-document="$0.tmp" $UPDATE_BASE/$SELF ; then
@@ -87,14 +94,14 @@ runSelfUpdate() {
     exit 1
   fi
   echo "Done."
-  
+
   # Copy over modes from old version
   OCTAL_MODE=$(stat -c '%a' $SELF)
   if ! chmod $OCTAL_MODE "$0.tmp" ; then
     echo "Failed: Error while trying to set mode on $0.tmp."
     exit 1
   fi
-  
+
   # Spawn update script
   cat > updateScript.sh << EOF
 #!/bin/bash
@@ -106,7 +113,7 @@ else
   echo "Failed!"
 fi
 EOF
-  
+
   echo -n "Inserting update process..."
   exec /bin/bash updateScript.sh
 }
@@ -133,6 +140,12 @@ for option in $*; do
       ;;
     --version=*)
       VERSION=$(echo $option | cut -d'=' -f2)
+      ;;
+    --skip-gm-detect)
+      SKIP_GM_DETECT=true
+      ;;
+    --skip-unzip-detect)
+      SKIP_UNZIP_DETECT=true
       ;;
     --hostname=*)
       HOST=$(echo $option | cut -d'=' -f2)
@@ -202,12 +215,48 @@ fi
 echo "Done."
 
 # Generate configuration
+
+# Print a single newline, but only the first time it is called
+_NEWLINE_PRINTED=false
+newLineOnce() {
+  if $_NEWLINE_PRINTED; then
+      return
+  fi
+  echo
+  _NEWLINE_PRINTED=true
+}
+
 echo -n "Generating localconf.php..."
 TYPO3_CONFIG=
+
+# Add database configuration
 TYPO3_CONFIG=$TYPO3_CONFIG"\$typo_db_username = '$USER';\n"
 TYPO3_CONFIG=$TYPO3_CONFIG"\$typo_db_password = '$PASS';\n"
 TYPO3_CONFIG=$TYPO3_CONFIG"\$typo_db_host     = '$HOST';\n"
 TYPO3_CONFIG=$TYPO3_CONFIG"\$typo_db          = '$DB';\n"
+
+# Add GraphicsMagick (if available)
+if ! $SKIP_GM_DETECT; then
+  if ! hash gm 2>&-; then
+    newLineOnce
+    echo "  Could not find GraphicsMagick binary. im_version_5 will not be set."
+  else
+    LOCATION_GM=$(which gm)
+    TYPO3_CONFIG=$TYPO3_CONFIG"\$TYPO3_CONF_VARS['GFX']['im_version_5'] = '$LOCATION_GM';\n"
+  fi
+fi
+
+# Add unzip utility
+if ! $SKIP_UNZIP_DETECT; then
+  if ! hash unzip 2>&-; then
+    newLineOnce
+    echo "  Could not find unzip binary. unzip_path will not be set."
+  else
+    LOCATION_UNZIP=$(which unzip)
+    TYPO3_CONFIG=$TYPO3_CONFIG"\$TYPO3_CONF_VARS['BE']['unzip_path'] = '$LOCATION_UNZIP';\n"
+  fi
+fi
+
 # Write configuration
 if ! cp $BASE/typo3conf/localconf.php $BASE/typo3conf/localconf.php.orig; then
   echo "Failed! Unable to create copy of localconf.php"
