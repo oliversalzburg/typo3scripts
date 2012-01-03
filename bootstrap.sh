96493dc23de170d2676150c6444a0529c3fcf844
oliversalzburg <oliver.salzburg@gmail.com>
2012-01-03 15:47:22 +0000

    Fixed a critical logic error when deciding if config file should be sourced.
    git-svn-id: http://typo3scripts.googlecode.com/svn/branches/dev@88 a8a2147e-0eb0-e98b-0563-54e275c8f842

--- bootstrap.sh
+++ bootstrap.sh
@@ -141,7 +141,7 @@ EOF
 
 # Read external configuration (overwrites default, hard-coded configuration)
 CONFIG_FILENAME=${SELF:0:${#SELF}-3}.conf
-if [[ -e "$CONFIG_FILENAME" && $# > 1 && "$1" != "--help" && "$1" != "-h" ]]; then
+if [[ -e "$CONFIG_FILENAME" && !( $# > 1 && "$1" != "--help" && "$1" != "-h" ) ]]; then
   echo -n "Sourcing script configuration from $CONFIG_FILENAME..."
   source $CONFIG_FILENAME
   echo "Done."
