628493db35a41844db9c4c0c9520b9c842e4f289
oliversalzburg <oliver.salzburg@gmail.com>
2012-02-11 14:24:20 +0000

    Introducing extExtract.php. Currently common, core functionality of typo3scripts is being ported to this PHP version.
    git-svn-id: http://typo3scripts.googlecode.com/svn/branches/dev@125 a8a2147e-0eb0-e98b-0563-54e275c8f842

---
+++ extExtract.php
@@ -0,0 +1,97 @@
+#!/usr/bin/php
+<?php
+// TYPO3 Extension Extraction Script
+// written by Oliver Salzburg
+
+define( "SELF", basename( __FILE__ ) );
+define( "INVNAME", $argv[ 0 ] );
+  
+/**
+ * Show the help for this script
+ * @name string The name of this script as invoked by the user (usually $argv[0])
+ */
+function showHelp( $name ) {
+  echo <<<EOS
+  Usage: $name [OPTIONS]
+  
+  Core:
+  --help              Display this help and exit.
+  --update            Tries to update the script to the latest version.
+  --base=PATH         The name of the base path where Typo3 is
+                    installed. If no base is supplied, "typo3" is used.
+  --export-config     Prints the default configuration of this script.
+  --extract-config    Extracts configuration parameters from TYPO3.
+  
+  Options:
+  --extension=EXTKEY  The extension key of the extension that should be
+                      operated on.
+EOS;
+}
+
+/**
+ * Print the default configuration to ease creation of a config file.
+ */
+function exportConfig() {
+  $config = "";
+  preg_match_all( "/# Script Configuration start.+?# Script Configuration end/ms", file_get_contents( INVNAME ), $config );
+  echo $config[ 0 ][ 1 ];
+}
+
+function extractConfig() {
+  echo "Extracting a configuration is currently not supported!\n";
+  return;
+
+  $LOCALCONF = $BASE . "/typo3conf/localconf.php";
+  $_configFileContent = file_get_contents( $LOCALCONF );
+
+  $_hostMatch = "";
+  preg_match_all( "/(?<=typo_db_host = ')[^']*(?=';)/", $_configFileContent, $_hostMatch );
+}
+
+define( "REQUIRED_ARGUMENT_COUNT", 0 );
+if( $argc < REQUIRED_ARGUMENT_COUNT ) {
+  file_put_contents( "php://stderr", "Insufficient command line arguments!" );
+  file_put_contents( "php://stderr", "Use INVNAME --help to get additional information." );
+  exit( 1 );
+}
+
+# Script Configuration start
+// The base directory where Typo3 is installed
+$BASE="typo3";
+// The hostname of the MySQL server that Typo3 uses
+$HOST="localhost";
+// The username used to connect to that MySQL server
+$USER="*username*";
+// The password for that user
+$PASS="*password*";
+// The name of the database in which Typo3 is stored
+$DB="typo3";
+// The extension key for which to retrieve the changelog
+$EXTENSION="";
+# Script Configuration end
+
+// The base location from where to retrieve new versions of this script
+define( "UPDATE_BASE", "http://typo3scripts.googlecode.com/svn/trunk" );
+
+/**
+ * Self-update
+ */
+function runSelfUpdate() {
+  echo "Performing self-update...\n";
+
+  // Download new version
+  echo "Downloading latest version...";
+  $_fileContents = @file_get_contents( UPDATE_BASE . "/" . SELF );
+  if( strlen( $_fileContents ) <= 0 ) {
+    echo "Failed: Error while trying to download new version!\n";
+    echo "File requested: " . UPDATE_BASE . "/" . SELF . "\n";
+    exit( 1 );
+  }
+}
+
+#showHelp( $argv[ 0 ] );
+#exportConfig();
+runSelfUpdate();
+
+# vim:ts=2:sw=2:expandtab:
+?>
